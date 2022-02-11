0fbbc66e893b7e6907346b289418c6eb42558748
Lasse96 <lasse_strand_96@hotmail.com>
2022-02-11 12:46:26 +0100

    MA3

---
+++ question-2.js
@@ -0,0 +1,26 @@
+const url = "https://api.rawg.io/api/games?dates=2019-01-01,2019-12-31&ordering=-rating&key=e452999af1e441efa51b1461d342e361";
+const out = document.querySelector("#out");
+const cors = "https://noroffcors.herokuapp.com/" + url;
+
+listGames = (list) => {
+    console.log(list);
+    out.innerHTML = "";
+    if (list <8) {
+    for (let item of list) {
+        let newdiv = `<div>
+        <h2>${item.name}</h2>
+        <p>${item.rating}</p>
+        <p>${item.tags.length}</p>
+        </div>`;
+        out.innerHTML += newdiv;
+    }}
+};
+
+fetch(cors)
+    .then(response => response.json())
+    .then(data => listGames(data.results))
+    .catch(error =>{
+        console.error(error.message);
+        out.innerHTML =`<div class="error> Something wrong!</div>`;
+    });
+
